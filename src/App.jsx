8101da08edae58aa1ecc6cb22da71da177fd3c44
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2025-11-14 21:56:18 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import WorkoutPlanner from "./pages/WorkoutPlanner"
+import Aurora from "./Aurora"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/planner" element={<WorkoutPlanner />} />
-    </Routes>
+    <>
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.5}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/planner" element={<WorkoutPlanner />} />
+      </Routes>
+    </>
   )
 }
